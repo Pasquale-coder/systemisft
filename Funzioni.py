74352f15b80c90a8c188230888918d2713d60a56
pasquale <omostr3@gmail.com>
2022-11-08 18:27:43 +0100

    cambiamento funzioni

--- Funzioni.py
+++ Funzioni.py
@@ -1,6 +1,41 @@
+from tkinter import *
 import tkinter as tk
 import tkinter.messagebox
 import datetime
+
+"""
+creazione funzione per la generazione delle pagine
+def finestra_clienti():
+  clienti=TK()
+  clienti.geometry("1000*1000")
+  clienti.title("Clienti")
+  
+  b1=Button(w1,text="A & L Assistenza elettrodomestici Lombardia SNC",command=lambda:dati(mydb,1)).pack(pady=10)
+  w1.mainloop()
+
+def dati(mydb,id):
+   w2 = Toplevel()
+   w2.title(Funzioni.EC_cliente(mydb,id))
+   w2.geometry("1000x1000")
+   
+   #generazione label ed entry dei toner
+   toner(mydb,id,"nero")
+   toner(mydb,id,"ciano")
+   toner(mydb,id,"magenta")
+   toner(mydb,id,"giallo")
+   
+def toner(mydb,id,colore):
+  if colore=="nero":
+   
+  elif colore=="ciano":
+  
+  elif colore=="magenta":
+  
+  elif colore=="giallo":
+   
+   
+  
+"""
 def EC_cliente(mydb,id):
     mycursor = mydb.cursor(mydb)
     mycursor.execute("SELECT Nome_CLiente FROM Clienti where ID_Cliente="+str(id))
@@ -68,25 +103,10 @@ def EC_scorta(mydb,id):
 
 
 def aggiorna_database(mydb,id,cliente,dataconsegna,contatore,previsione,datascorta,scorta):
-   bool fcliente,fdataconsegna,fcontatore,fprevisione,fdatascorta,fscorta=False
-   fcliente=True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """conferma=tk.messagebox.askquestion('conferma','stai per aggiornare i seguenti campi, sei sicuro?',icon='warning')
+    fcliente,fdataconsegna,fcontatore,fprevisione,fdatascorta,fscorta=(True,)*6
+    #fcliente=True
+    """
+    conferma=tk.messagebox.askquestion('conferma','stai per aggiornare i seguenti campi, sei sicuro?',icon='warning')
     if conferma=='yes':
         print(confermato)
     else:
